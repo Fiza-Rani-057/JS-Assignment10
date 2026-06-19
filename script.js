7886f104fa88dbe86a7b31a8af790b5c77e0e629
Fiza-Rani-057 <fahadsaeed057@gmail.com>
2026-06-19 23:18:53 +0500

    String for numbers

--- script.js
+++ script.js
@@ -6,6 +6,7 @@
  var fullName = firstname + " " + lastname;
 
     document.write("Question 1" + "<br>" + "<br>");
+
    document.write("Hello" + " " + fullName  +"<br>" + "<br>");
 
 //  Question 2
@@ -14,18 +15,30 @@
 
 
    document.write("Question 2" + "<br>" + "<br>");
+
  document.write("My Favorite Mobile phone Model is:"+ favoritePhone +
   "<br>");
   document.write("Length of string is:"  +  favoritePhone.length + "<br>" 
     + "<br>");
 
 //   Question 3
+
  var word = "Pakistani";
  
  document.write("Question 3"  +"<br>" + "<br>");
+
  document.write("String:" + " " +  word  + "<br>");
- document.write("Index of String 'n':"+ word.indexOf( "n"));
+ document.write("Index of String 'n':"+ word.indexOf( "n")+ "<br>" + "<br>");
 
 //   Question 4
+
+ var word2 = "Hello world";
+
+ document.write("Question 4" + "<br>" + "<br>");
+
+ document.write("String:" + " " + word2 + "<br>");
+ document.write("Last Index of 'l' is:"  + word2.lastIndexOf("l") 
+  +"<br>" + "<br>");
+
 
 
