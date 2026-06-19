fc8e7943bbf8824ea6455a09787f5612b30b64ed
Fiza-Rani-057 <fahadsaeed057@gmail.com>
2026-06-19 23:33:23 +0500

    String for numbers

--- script.js
+++ script.js
@@ -5,7 +5,7 @@
  var lastname = prompt("Enter your last name");
  var fullName = firstname + " " + lastname;
 
-    document.write("Question 1" + "<br>" + "<br>");
+    document.write( "<h1>" + "Question 1" + "<br>" + "<br>" + "</h1>");
 
    document.write("Hello" + " " + fullName  +"<br>" + "<br>");
 
@@ -14,7 +14,7 @@
  var favoritePhone = prompt("What is your favorite Mobile phone Model?");
 
 
-   document.write("Question 2" + "<br>" + "<br>");
+   document.write( "<h1>" +"Question 2" + "<br>" + "<br>" + "</h1>");
 
  document.write("My Favorite Mobile phone Model is:"+ favoritePhone +
   "<br>");
@@ -25,7 +25,7 @@
 
  var word = "Pakistani";
  
- document.write("Question 3"  +"<br>" + "<br>");
+ document.write( "<h1>" +"Question 3"  +"<br>" + "<br>" + "</h1>");
 
  document.write("String:" + " " +  word  + "<br>");
  document.write("Index of String 'n':"+ word.indexOf( "n")+ "<br>" + "<br>");
@@ -34,11 +34,25 @@
 
  var word2 = "Hello world";
 
- document.write("Question 4" + "<br>" + "<br>");
+ document.write( "<h1>" + "Question 4" + "<br>" + "<br>" + "</h1>");
 
  document.write("String:" + " " + word2 + "<br>");
  document.write("Last Index of 'l' is:"  + word2.lastIndexOf("l") 
   +"<br>" + "<br>");
+
+// Question 5
+
+var word3 = "Pakistani";
+
+ document.write( "<h1>" + "Question 5" + "<br>" + "<br>" + "</h1>");
+
+document.write("String:" + word2 + "<br>");
+document.write("Character at Index 3:" + word3.charAt(3) + "<br>" + "<br>" );
+
+// Question 6
+
+
+
 
 
 
