bd3e98c9f46557b80a4b5a61d92b8ab770e77de5
Fiza-Rani-057 <fahadsaeed057@gmail.com>
2026-06-19 22:35:59 +0500

    string for numbers

---
+++ script.js
@@ -0,0 +1,14 @@
+//  String for numbers 
+//  Question 1
+
+ var firstname = prompt("Enter your first name");
+ var lastname = prompt("Enter your last name");
+ var fullName = firstname + " " + lastname;
+
+   document.write("Hello" + " " + fullName);
+
+//  Question 2
+ 
+ 
+
+
