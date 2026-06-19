41dec66924ff4cef96c643cc937a4ee0d9f5ec0e
Fiza-Rani-057 <fahadsaeed057@gmail.com>
2026-06-19 23:11:01 +0500

    string for nubers

--- script.js
+++ script.js
@@ -5,10 +5,27 @@
  var lastname = prompt("Enter your last name");
  var fullName = firstname + " " + lastname;
 
-   document.write("Hello" + " " + fullName);
+    document.write("Question 1" + "<br>" + "<br>");
+   document.write("Hello" + " " + fullName  +"<br>" + "<br>");
 
 //  Question 2
  
+ var favoritePhone = prompt("What is your favorite Mobile phone Model?");
+
+
+   document.write("Question 2" + "<br>" + "<br>");
+ document.write("My Favorite Mobile phone Model is:"+ favoritePhone +
+  "<br>");
+  document.write("Length of string is:"  +  favoritePhone.length + "<br>" 
+    + "<br>");
+
+//   Question 3
+ var word = "Pakistani";
  
+ document.write("Question 3"  +"<br>" + "<br>");
+ document.write("String:" + " " +  word  + "<br>");
+ document.write("Index of String 'n':"+ word.indexOf( "n"));
+
+//   Question 4
 
 
